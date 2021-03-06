5daacf5e81f30702b16bb25c2639755312e6d078
Brach Sebastien <sebastien.brach@etu.umontpellier.fr>
2021-03-06 22:50:31 +0100

    AdaptateurDAOXML OK

--- src/DAL/AdaptateurDAOXML.java
+++ src/DAL/AdaptateurDAOXML.java
@@ -6,9 +6,8 @@ import java.util.List;
 import Metier.I_Produit;
 
 /**
- * 
- * @author Sébastien
- *
+ * AdaptateurDAOXML
+ * Permet d'adapter le code de ProduitDAOXML avec le bon nom des méthodes
  */
 public class AdaptateurDAOXML implements I_ProduitDAO {
 
@@ -18,53 +17,33 @@ public class AdaptateurDAOXML implements I_ProduitDAO {
 		this.produitXML = new ProduitDAOXML();
 	}
 
-	/**
-	 * 
-	 */
-	@Override
 	public boolean create(I_Produit produit) throws DAOException {
 		return this.produitXML.creer(produit);
 	}
 	
-	/**
-	 * 
-	 */
 	@Override
 	public int createAll(List<I_Produit> produits) throws DAOException, SQLException {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	/**
-	 * 
-	 */
 	@Override
 	public I_Produit readByName(String nomProduit) throws DAOException, SQLException {
 		return this.produitXML.lire(nomProduit);
 	}
 	
-	/**
-	 * 
-	 */
 	@Override
 	public List<I_Produit> read() throws DAOException, SQLException {
 		return this.produitXML.lireTous();
 	}
 
-	/**
-	 * 
-	 */
 	@Override
 	public boolean update(I_Produit produit) throws DAOException, SQLException {
 		return this.produitXML.maj(produit);
 	}
 
-	/**
-	 * 
-	 */
 	@Override
 	public boolean delete(I_Produit produit) throws DAOException, SQLException {
 		return this.produitXML.supprimer(produit);
 	}
-
 }
